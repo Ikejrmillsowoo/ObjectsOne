416000d0027513af5d74e84b9eb44eea798d992d
Isaac <isaac@Zipcoders-MacBook-Pro-2.local>
2025-03-04 16:27:26 -0500

    created car and dog class

--- object1/src/main/java/object1/Person.java
+++ object1/src/main/java/object1/Person.java
@@ -3,17 +3,19 @@ package object1;
 public class Person {
     String name;
     int age = 0;
+    int height = 0; //Height is in centimeters
 
     // Constructor
-    public Person(String name, int age) {
+    public Person(String name, int age, int height) {
         this.name = name;
         this.age = age;
+        this.height = height;
     }
 
     public static void main(String[] args) {
         // Declare and initialize an object
-        Person person1 = new Person("John", 30);
-
+        Person person1 = new Person("James", 28, 300);
+        System.out.println(person1.height + " " + "centimeters");
         /* Now you can use the Object `person1` */
 
     }
