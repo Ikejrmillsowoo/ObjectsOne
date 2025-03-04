c23c3e4839d5e96475b173d74e9ee888ecb267d2
Isaac <isaac@Zipcoders-MacBook-Pro-2.local>
2025-03-04 17:01:22 -0500

    updated chair and create arrays of cars, chairs and dogs

--- object1/src/main/java/object1/Dog.java
+++ object1/src/main/java/object1/Dog.java
@@ -30,4 +30,10 @@ public class Dog {
         public String getBreed() {
             return breed;
         }
+    public void setBreed(String breed) {
+        this.breed = breed;
+
+}
+
+
 }
--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -1,5 +1,6 @@
 package object1;
 
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 public class Main {
@@ -7,6 +8,34 @@ public class Main {
     public static void main(String... args) {
         Logger logger=Logger.getLogger(Main.class.getName());
         logger.info("This is a module-using Hello World!");
+
+        Dog [] dogs = new Dog[10];
+        Dog dog1 = new Dog("Scooby", 10, "white");
+        Dog dog2 = new Dog("Scrappy", 12, "black");
+
+        dogs[0] = dog1;
+        dogs[5] = dog2;
+
+        System.out.println(dog1.getName());
+
+        Chair [] chairs = new Chair[10];
+        Chair chair1 = new Chair("Stool", "blue", 3);
+        Chair chair2 = new Chair("Couch", "red", 2);
+
+        chairs[0] = chair1;
+        chairs[5] = chair2;
+
+        System.out.println(chair2.getColor());
+
+        Car [] car = new Car[10];
+        Car car1 = new Car("Kia", "Optima", 2016, 160000);
+        Car car2 = new Car("VW", "Tiguan", 2019, 80000);
+
+        car[0] = car1;
+        car[5] = car2;
+
+        System.out.println(car2.getMake());
+
 
     }
 }
